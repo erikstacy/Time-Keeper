db82e8d818c0bd436245217190005dd8d4d997a2
erikstacy <erikslaststand@gmail.com>
2020-02-18 17:15:47 -0600

    Add, edit, and delete Categories in the EditCategoryScreen

--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,6 +1,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:time_keeper/services/models.dart';
 
 class DatabaseService {
 
@@ -12,6 +13,39 @@ class DatabaseService {
       'startTime': DateTime.now(),
       'endTime': DateTime.utc(1960, 1, 1, 12, 0, 0),
     });
+  }
+
+  /*
+
+    Streams
+
+  */
+
+  Stream<List<Category>> streamCategoryList(FirebaseUser user) {
+    var ref = _db.collection('users').document(user.uid).collection('categories');
+    return ref.snapshots().map((list) => list.documents.map((doc) => Category.fromFirestore(doc)).toList());
+  }
+
+  /*
+
+    Writes
+
+  */
+
+  void addCategory(FirebaseUser user, String title) {
+    _db.collection('users').document(user.uid).collection('categories').add({
+      'title': title,
+    });
+  }
+
+  void editCategory(FirebaseUser user, String categoryId, String title) {
+    _db.collection('users').document(user.uid).collection('categories').document(categoryId).setData({
+      'title': title,
+    }, merge: true);
+  }
+
+  void deleteCategory(FirebaseUser user, String categoryId) {
+    _db.collection('users').document(user.uid).collection('categories').document(categoryId).delete();
   }
 
 }
