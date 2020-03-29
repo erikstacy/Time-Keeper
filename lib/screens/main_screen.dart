3612f71b05d3ce049fd4dd2ee828e2a073f8bfc6
erikstacy <erikslaststand@gmail.com>
2020-03-29 13:48:33 -0500

    Created the first three cards on the Main Page, and started working on the Drawer

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:time_keeper/screens/categories_screen.dart';
 import 'package:time_keeper/screens/current_activity_screen.dart';
@@ -17,47 +18,429 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
 
-  int selectedPage = 0;
-  final _pageOptions = [
-    CurrentActivityScreen(),
-    TotalsScreen(),
-    CategoriesScreen(),
-    ProfileScreen(),
-  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Time Keeper'),
+          backgroundColor: Colors.black,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            CurrentActivityCard(),
+            NewDayCard(),
+            TotalsCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Stack(
+              children: [
+                Positioned(
+                  bottom: 12,
+                  left: 16,
+                  child: Text(
+                    'Time Keeper',
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.purple,
+                  Colors.grey[800],
+                  Colors.black,
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.cubes),
+            title: Text('Categories'),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.signOutAlt),
+            title: Text('Sign Out'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CurrentActivityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnimatedContainer(
-        duration: Duration(seconds: 1),
-        child: _pageOptions[selectedPage],
+    return GestureDetector(
+      onTap: () {
+        // Todo - Start a new task
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.grey[700],
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.walking,
+                    color: Colors.blue,
+                    size: 18,
+                  ),
+                  SizedBox(width: 10,),
+                  Text(
+                    'Current Activity',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.grey[850],
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '3:56',
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          Text(
+                            'TOTAL TIME',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                'CATEGORY',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey[400],
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.3,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                'Coding',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                'START TIME',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey[400],
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.3,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(
+                                '1:28',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedPage,
-        type: BottomNavigationBarType.fixed,      
-        items: [
-          BottomNavigationBarItem(
-            title: Text("Activity"),
-            icon: Icon(Icons.directions_run),
-          ),
-          BottomNavigationBarItem(
-            title: Text("Totals"),
-            icon: Icon(Icons.calendar_today),
-          ),
-          BottomNavigationBarItem(
-            title: Text("Categories"),
-            icon: Icon(Icons.category),
-          ),
-          BottomNavigationBarItem(
-            title: Text("Profile"),
-            icon: Icon(Icons.person),
-          ),
-        ],
-        onTap: (int id) {
-          setState(() {
-            selectedPage = id;
-          });        
-        },
+    );
+  }
+}
+
+class NewDayCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Todo - Start a new day
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.grey[700],
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.calendarDay,
+                    color: Colors.purpleAccent,
+                    size: 18,
+                  ),
+                  SizedBox(width: 10,),
+                  Text(
+                    'Start New Day',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.grey[850],
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(left: 20),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'Start a ',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'NEW DAY ',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.purpleAccent,
+                      ),
+                    ),
+                    Text(
+                      'here!',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TotalsCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Todo - Go to totals screen
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.grey[700],
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.clock,
+                    color: Colors.yellowAccent,
+                    size: 18,
+                  ),
+                  SizedBox(width: 10,),
+                  Text(
+                    'Total Time',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.grey[850],
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 20,),
+                    child: Text(
+                      'Today\'s Totals',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Coding',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[400],
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 0.3,
+                              ),
+                            ),
+                            SizedBox(height: 5,),
+                            Text(
+                              '4:00',
+                              style: TextStyle(
+                                color: Colors.yellowAccent,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Family',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[400],
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 0.3,
+                              ),
+                            ),
+                            SizedBox(height: 5,),
+                            Text(
+                              '3:32',
+                              style: TextStyle(
+                                color: Colors.yellowAccent,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Entertainment',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.grey[400],
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 0.3,
+                              ),
+                            ),
+                            SizedBox(height: 5,),
+                            Text(
+                              '2:48',
+                              style: TextStyle(
+                                color: Colors.yellowAccent,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
